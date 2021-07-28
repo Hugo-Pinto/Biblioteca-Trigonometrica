a7ab57cc31ec64c2dbb52de4e2594026f7e37bd0
Hugo-Pinto <55241066+Hugo-Pinto@users.noreply.github.com>
2021-07-28 18:42:56 -0300

    Create trigo.h

---
+++ trigo.h
@@ -0,0 +1,14 @@
+#ifndef TRIGO_H//TRIGO.H
+#define TRIGO_H
+#include <stdio.h>
+#include <string.h>
+#define PI 3.1415
+#define termos 10
+double grausRadianos(double x);
+double fat(double a);
+double potencia(double base, double expoente);
+double cosseno(double x);
+double seno(double x);
+double tangente(double x);
+ 
+#endif
